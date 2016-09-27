ff6bae6d6080404ccd97e1f2cb8ec4b298bade5c
Tommy <tjanicik@gmail.com>
2016-09-26 23:50:38 -0500

    initial migration && add Room and Amenity models

---
+++ HotelMVC/Models/Amenity.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelMVC.Models
+{
+    public class Amenity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ HotelMVC/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelMVC.Models
+{
+    public class Room
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public byte Id { get; set; }
+        public List<Amenity> Amenities { get; set; }
+    }
+}
